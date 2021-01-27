baa46d663c78b3fbd080a2c4d436219629e7ad00
gengu6585 <2352462017@qq.com>
2021-01-27 18:32:06 +0800

    添加helloworld

--- src/main/java/com/zwj/git_springboot_study/GitSpringbootStudyApplication.java
+++ src/main/java/com/zwj/git_springboot_study/GitSpringbootStudyApplication.java
@@ -8,6 +8,7 @@ public class GitSpringbootStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitSpringbootStudyApplication.class, args);
+        System.out.println("helloworld");
     }
 
 }
