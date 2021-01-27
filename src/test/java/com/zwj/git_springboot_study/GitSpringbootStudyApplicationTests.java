b499149d2c1d9fd97585633be48d402625ada926
gengu6585 <2352462017@qq.com>
2021-01-27 16:58:25 +0800

    导入版本控制后项目后第一次提交

---
+++ src/test/java/com/zwj/git_springboot_study/GitSpringbootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwj.git_springboot_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSpringbootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
